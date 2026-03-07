ec245a97432d7af0792e39eefa28c6a01639706c
NBalboa <nickojek2x@gmail.com>
2026-03-08 04:38:14 +0800

    Bug Fix: Expenses Order

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :date, presence: true
 end
