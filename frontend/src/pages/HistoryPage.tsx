e246364317804e0120fe5d0cb5e51f5a9739f304
NBalboa <nickojek2x@gmail.com>
2026-03-08 19:03:59 +0800

    Refactor: separate category api services

--- frontend/src/pages/HistoryPage.tsx
+++ frontend/src/pages/HistoryPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { fetchCategories, createCategory } from "../services/api";
 import { Category, CategoryFormData, Expense, ExpenseFormData } from "../types";
 import YearNavigation from "../components/YearNavigation";
 import { MonthNavigation } from "../components/MonthNavigation";
@@ -10,6 +9,7 @@ import { Modal, Button } from "../vibes";
 import { COLORS } from "../constants/colors";
 import { CategoryForm } from "../components/CategoryForm";
 import { createExpense, getExpenses } from "../services/expenseApi";
+import { createCategory, fetchCategories } from "../services/categoryApi";
 
 const HistoryPage: React.FC = () => {
   const [expenses, setExpenses] = useState<Expense[]>([]);
