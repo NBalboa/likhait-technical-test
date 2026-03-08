9f6c4f8a1a7895e3cccbcd91f17fdf57fe56db59
NBalboa <nickojek2x@gmail.com>
2026-03-08 13:05:17 +0800

    Fix: handle future dates

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -4,5 +4,13 @@ class Expense < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :category_id, presence: true
-  validates :date, presence: true, comparison: { less_than_or_equal_to: Date.today }
+  validates :date, presence: true
+  validate :no_future_date
+
+  private
+  def no_future_date
+    if date > Date.today
+      errros.add(:date, "Date must not be in the future")
+    end
+  end
 end
