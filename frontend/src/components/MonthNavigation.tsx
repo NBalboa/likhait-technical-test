c7d80099b0c38f9589b522f27aec2309a52779e3
NBalboa <nickojek2x@gmail.com>
2026-03-08 19:52:25 +0800

    Refactor: put months in the constant folder

--- frontend/src/components/MonthNavigation.tsx
+++ frontend/src/components/MonthNavigation.tsx
@@ -6,27 +6,13 @@ import React from "react";
 import { COLORS } from "../constants/colors";
 import { navigationButtonStyle, wrapperStyle } from "../styles/monthNavigationStyle";
 import { containerStyle } from "../styles/categoryBreakdownStyle";
+import { MONTHS } from "../constants/months";
 
 interface MonthNavigationProps {
   currentMonth: number;
   currentYear: number;
   onMonthChange: (month: number, year: number) => void;
 }
-
-const MONTHS = [
-  { label: "Jan", value: 1 },
-  { label: "Feb", value: 2 },
-  { label: "Mar", value: 3 },
-  { label: "Apr", value: 4 },
-  { label: "May", value: 5 },
-  { label: "Jun", value: 6 },
-  { label: "Jul", value: 7 },
-  { label: "Aug", value: 8 },
-  { label: "Sep", value: 9 },
-  { label: "Oct", value: 10 },
-  { label: "Nov", value: 11 },
-  { label: "Dec", value: 12 },
-];
 
 export function MonthNavigation({
   currentMonth,
