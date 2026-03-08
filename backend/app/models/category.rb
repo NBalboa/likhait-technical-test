51e7785bfba4bd00bbc1ccb3a0f093a94c48526f
NBalboa <nickojek2x@gmail.com>
2026-03-08 17:53:40 +0800

    Refactor: change category api responses to error codes

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :expenses, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: { message: "of category is already taken", case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
